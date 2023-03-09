ee2b3b3196ddeb5931adff821fa918d35fb07a50
Joanne Pagounis <72542408+p19pago@users.noreply.github.com>
2023-03-09 22:56:04 +0200

    Update lab2_ex2_alt.py

--- Labs/Lab2/Python/ex2 alt solution/lab2_ex2_alt.py
+++ Labs/Lab2/Python/ex2 alt solution/lab2_ex2_alt.py
@@ -13,8 +13,8 @@ from numpy import sqrt
 # first we will load the data from our CSV file (namely 'temp-dataset.csv')
 # declaring our data as a variable named data
 
-# in this case, I used the official directory where my CSV file is located and imported
-# having cloned my github repository on my docs folder
+# in this case, I will not use the official directory where my CSV file is located and imported
+# just the file as an input depending on where it is located
 
 df = pd.read_csv("temp-dataset.csv",
                  sep=";",
